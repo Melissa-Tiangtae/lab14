acfb6e6f49b73931c386e398626a331a92b24359
Melissa Tiangtae <melissa_tiangate@cmu.ac.th>
2025-01-28 13:57:21 +0700

    lab14_1_completed

--- lab14_1.cpp
+++ lab14_1.cpp
@@ -4,11 +4,36 @@ using namespace std;
 template <typename T>
 void insertionSort(T d[],int N){
 
+	T hold;
+
+	for(int i=0; i<N-1; i++){
+
+		for(int j=i; j>=0; j--){
+
+				if(d[j+1]>d[j]){
+			hold = d[j+1];
+			d[j+1] = d[j];
+			d[j] = hold; }
+
+		}
+		cout << "Pass " << i+1 << ":";
+	for(int i = 0; i < 10; i++) 	cout << d[i] << " ";
+		cout << endl;
+
+
+		
+
+	}
+
+
+
 }
 
 int main(){
 	int a[10] = {12,25,30,44,2,0,4,7,55,25};
 	cout << "Input Array:";
+
+
 	for(int i = 0; i < 10; i++) cout << a[i] << " ";
 	cout << "\n\n";
 	
